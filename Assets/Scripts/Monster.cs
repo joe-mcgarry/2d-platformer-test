82be83a2de363bd48e6e03891de00d979f98e1ca
Joe McGarry <joewmcgarry@gmail.com>
2022-09-21 14:21:52 +0100

    Changes from box collider to capsule collider

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -13,7 +13,6 @@ public class Monster : MonoBehaviour
     {
         myBody = GetComponent<Rigidbody2D>();
 
-        speed = 7;
     }
 
     // Update is called once per frame
--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -8,15 +8,50 @@ public class MonsterSpawner : MonoBehaviour
     [SerializeField]
     private GameObject[] monsterReference;
 
+    private GameObject spawnedMonster;
+
+    [SerializeField]
+    private Transform leftPosition, rightPosition;
+
+    private int randomIndex;
+    private int randomSide;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnMonsters());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnMonsters()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1, 5));
+
+            randomIndex = Random.Range(0, monsterReference.Length);
+            randomSide = Random.Range(0, 2);
+
+            spawnedMonster = Instantiate(monsterReference[randomIndex]);
+
+            if (randomSide == 0)
+            {
+                // Left side
+                spawnedMonster.transform.position = leftPosition.position;
+                spawnedMonster.GetComponent<Monster>().speed = Random.Range(4, 10);
+            }
+            else
+            {
+                // Right
+                spawnedMonster.transform.position = rightPosition.position;
+                spawnedMonster.GetComponent<Monster>().speed = -Random.Range(4, 10);
+                spawnedMonster.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+        }
     }
 }
